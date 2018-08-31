29f7b13097e92dede6df3c72fa6418544d81ff0a
Rahul <r.rahul4511@gmail.com>
2018-08-31 13:07:40 +0530

    Searched article content view finished

---
+++ app/src/main/java/com/example/rahuldroid/project_news/SearchArticleActivity.java
@@ -0,0 +1,13 @@
+package com.example.rahuldroid.project_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_article);
+    }
+}
